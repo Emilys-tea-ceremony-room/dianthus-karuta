0bcc14fefbfa3acd3079fb4a82a81655f3868d7a
楓上綵為 <akgm3i@mmbp.local>
2019-06-18 00:09:56 +0900

    add images

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ export default class Index extends Vue {
 .background-image
   width 100%
   height 100%
-  background url('~assets/tearoom.jpg') no-repeat center
+  background url('~assets/background/tearoom.jpg') no-repeat center
   background-size cover
 
 .emily
